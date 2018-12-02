596994af34fd1a7d2eb9d355ab031fcb728ea296
mht <952326644@qq.com>
2018-12-02 22:45:49 +0800

    接口调试日志补充

--- src/main/java/com/group/coursesystem/controller/CourseController.java
+++ src/main/java/com/group/coursesystem/controller/CourseController.java
@@ -2,6 +2,8 @@ package com.group.coursesystem.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +15,14 @@ import com.group.coursesystem.service.CourseService;
 @RestController
 @RequestMapping("/courses")
 public class CourseController {
+    private static final Logger logger = LoggerFactory.getLogger(CourseController.class);
 
     @Autowired
     private CourseService courseSvc;
 
     @GetMapping()
     public List<Course> findAll() {
+        logger.info("收到查询全部课程请求，开始查询全部课程...");
         return courseSvc.findAllCourses();
     }
 }
--- src/main/java/com/group/coursesystem/service/impl/CourseServiceImpl.java
+++ src/main/java/com/group/coursesystem/service/impl/CourseServiceImpl.java
@@ -2,6 +2,8 @@ package com.group.coursesystem.service.impl;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,13 +13,16 @@ import com.group.coursesystem.service.CourseService;
 
 @Service(value = "courseSvc")
 public class CourseServiceImpl implements CourseService{
+    private static final Logger logger = LoggerFactory.getLogger(CourseServiceImpl.class);
     
     @Autowired
     private CourseRepository couseRep;
 
     @Override
     public List<Course> findAllCourses() {
-        return couseRep.findAll();
+        List<Course> allCourses = couseRep.findAll();
+        logger.info("查询全部课程 : " +  allCourses);
+        return allCourses;
     }
 
 }
